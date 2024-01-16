72db84622cac5dea5ecd48e9f55e5c07cb7d99c7
Radosław Rolka <radoslaw.rolka@gmail.com>
2024-01-16 16:14:32 +0100

    outline done

--- src/main/java/FEM/Main.java
+++ src/main/java/FEM/Main.java
@@ -1,7 +1,10 @@
 package FEM;
 
+import FEM.gui.App;
+import javafx.application.Application;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Application.launch(App.class, args);
     }
 }
